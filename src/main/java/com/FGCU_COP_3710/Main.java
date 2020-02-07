c61c2d041db1942ebc03ea310b7bf1f35707cee8
vladrph <54378636+vladrph@users.noreply.github.com>
2020-02-06 21:22:20 -0500

    Commit 2 Formatted Lines

--- src/main/java/com/FGCU_COP_3710/Main.java
+++ src/main/java/com/FGCU_COP_3710/Main.java
@@ -1,4 +1,5 @@
 package com.FGCU_COP_3710;
+
 import com.opencsv.CSVParser;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
@@ -12,40 +13,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, CsvValidationException {
-        try {
-                String path = "SEOExample";
-           // Scanner reader = new Scanner("SEOExample.csv");
+  public static void main(String[] args) throws IOException, CsvValidationException {
+    try {
+      String path = "SEOExample";
+      // Scanner reader = new Scanner("SEOExample.csv");
 
-           CSVReader temp = new CSVReader(new FileReader("SEOExample"));
+      CSVReader temp = new CSVReader(new FileReader("SEOExample"));
 
-            String []SEOExample;  // Creates ann array of values
-            while ((SEOExample = temp.readNext()) != null) {
+      String[] SEOExample;  // Creates ann array of values
+      while ((SEOExample = temp.readNext()) != null) {
 
-                System.out.println(SEOExample[0] +" "+ SEOExample[1]+" " +SEOExample[2] +" "+ SEOExample[3]+" "+SEOExample[4]
-                        + " "+SEOExample[5]+" "+SEOExample[6] +" "+ SEOExample[7]+" "+SEOExample[8] +" "+ SEOExample[9]);
+        System.out.println(
+            SEOExample[0] + " " + SEOExample[1] + " " + SEOExample[2] + " " + SEOExample[3] + " "
+                + SEOExample[4]
+                + " " + SEOExample[5] + " " + SEOExample[6] + " " + SEOExample[7] + " "
+                + SEOExample[8] + " " + SEOExample[9]);
 
+        System.out.println(
+            "--------------------------------------------------------------------------------------");
+      }
 
-                System.out.println("--------------------------------------------------------------------------------------");
-            }
-           // Reader reader = Files.newBufferedReader(Paths.get("SEOExample"));
-            //System.out.println(reader1.toString());
-            //System.out.println(reader);
-
-
-        /*    // System.out.println("Hello, I work ");
-            Reader reader = Files.newBufferedReader(Paths.get("src/Data/SEOExample"));
-            Path file = Paths.get("SEOExample");
-            // CSVReader reader = new CSVReader(new FileReader("src/Data/SEOExample"));
-
-            //  System.out.println(reader);
-            CSVReader read = new CSVReader(reader);
-            System.out.println(reader);
-            //System.out.println(read);*/
-            //System.out.print(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
 }
